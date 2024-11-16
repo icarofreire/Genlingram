e0b6d0849889fc7cf02ece60bfc5df8217d055f5
icaro <icarofre99@gmail.com>
2024-11-16 01:34:42 -0300

    escrito função de filtro de comandos de linha de commando e aplicados a excecução do projeto;

--- main.c
+++ main.c
@@ -49,6 +49,14 @@ void apply_files_rule(char *file_code, char *file_rules, const int lang){
 	free_dates_grammar_symbols(gsymbols);
 }
 
+int str_lang_to_const_enum(const char* lang){
+	if(strcmp(lang, "python")) return PYTHON;
+	else if(strcmp(lang, "ruby")) return RUBY;
+	else if(strcmp(lang, "js")) return JS;
+	else if(strcmp(lang, "java")) return JAVA;
+	return -1;
+}
+
 char* get_extension_lang(const int lang){
 	char* ext = (char *)malloc((10) * sizeof(char));
 	switch(lang){
@@ -99,10 +107,56 @@ void apply_rules_in_project(char* dir_project, char* dir_rules, const int lang){
 	free(extension);
 }
 
+void filter_commands(int argc, char **argv){
+	char *cmd = NULL;
+	char proj[100];
+	char lang[50];
+	char rules[100];
+	for (int i = 0; i < argc; i++) {
+
+		char* opt = "-proj=";
+		cmd = strstr(argv[i], opt);
+		if(cmd != NULL){
+			int len = strlen(argv[i]) - strlen(opt);
+			memcpy(proj, argv[i]+strlen(opt), len);
+			proj[len] = '\0';
+		}
+
+		opt = "-rules=";
+		cmd = strstr(argv[i], opt);
+		if(cmd != NULL){
+			int len = strlen(argv[i]) - strlen(opt);
+			memcpy(rules, argv[i]+strlen(opt), len);
+			rules[len] = '\0';
+		}
+
+		opt = "-lang=";
+		cmd = strstr(argv[i], opt);
+		if(cmd != NULL){
+			int len = strlen(argv[i]) - strlen(opt);
+			memcpy(lang, argv[i]+strlen(opt), len);
+			lang[len] = '\0';
+		}
+    }
+
+	if(
+		strcmp(proj, "") != 0 &&
+		strcmp(lang, "") != 0 &&
+		strcmp(rules, "") != 0
+	){
+		int lang_num = str_lang_to_const_enum(lang);
+		if(lang_num != -1){
+			apply_rules_in_project(proj, rules, lang_num);
+		}
+	}
+}
+
 // DRIVER FUNCTION
-int main()
+int main(int argc, char **argv)
 {
 	printf("Genlingram. ;)\n");
+
+	filter_commands(argc, argv);
 
 	// char *file_code = "testes/code-input.txt";
 	// char *file_code = "testes/AStarSearch.txt";
@@ -111,7 +165,7 @@ int main()
 	// char *file_code = "testes/code-example-js.txt";
 	// apply_files_rule("testes/code-example-js.txt", "rules/code-example-js.txt", JS);
 
-	apply_rules_in_project("rules", "rules", JS);
+	// apply_rules_in_project("rules", "rules", JS);
 
 
 	return (0);
